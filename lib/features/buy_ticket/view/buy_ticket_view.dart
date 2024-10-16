236cd872fb19cb973afe6bbdd12952cc694a82a4
berke.yz <berkeyilmaz189@gmail.com>
2024-10-16 16:08:12 +0300

    view

---
+++ lib/features/buy_ticket/view/buy_ticket_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BuyTicketView extends StatefulWidget {
+  const BuyTicketView({super.key});
+
+  @override
+  State<BuyTicketView> createState() => _BuyTicketViewState();
+}
+
+class _BuyTicketViewState extends State<BuyTicketView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
