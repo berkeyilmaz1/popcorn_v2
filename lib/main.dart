faaaa36d48c4d5e8981bbcecfa36871459908c76
berke.yz <berkeyilmaz189@gmail.com>
2024-10-18 18:08:55 +0300

    app start folder created

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
-import 'package:easy_localization/easy_localization.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:popcorn_v2/firebase_options.dart';
+import 'package:popcorn_v2/product/initialize/application_start/application_start.dart';
 import 'package:popcorn_v2/product/initialize/localization/main_localization.dart';
 import 'package:popcorn_v2/product/initialize/main_app/main_app.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await EasyLocalization.ensureInitialized();
+  await ApplicationStart.init();
 
   runApp(
     MainLocalization(child: const MainApp()),
---
+++ lib/product/initialize/application_start/application_start.dart
@@ -0,0 +1,18 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:popcorn_v2/firebase_options.dart';
+import 'package:popcorn_v2/product/initialize/cache/auth_cache.dart';
+
+final class ApplicationStart {
+  const ApplicationStart._();
+  static Future<void> init() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    await EasyLocalization.ensureInitialized();
+
+    await AuthCache.instance.setup();
+  }
+}
