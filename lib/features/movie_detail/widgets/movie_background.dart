6abc54f1a4c6d9c4636508b7a982ea7290b16efb
berke.yz <berkeyilmaz189@gmail.com>
2024-11-19 13:30:36 +0300

    remove blank

--- lib/features/movie_detail/widgets/movie_background.dart
+++ lib/features/movie_detail/widgets/movie_background.dart
@@ -28,7 +28,6 @@ final class MovieBackground extends StatelessWidget {
                   images?.filePath ?? '',
                 ),
               ),
-              // MoviePosterAndRating(widget: widget),
             ],
           );
         },
